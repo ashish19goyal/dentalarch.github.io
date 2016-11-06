5cd55450885b612d9641277575c85f13f41f7adf
Ashish Goyal <ashish.19goyal@gmail.com>
2016-11-06 20:05:05 +0530

    Book appointment updated
    Parallax removed

--- js/contact/contact-form.js
+++ js/contact/contact-form.js
@@ -2,65 +2,71 @@ $(function(){
 	$("#ContactForm").submit(function(event)
 	{
 		event.preventDefault();
-		
-		$("#submitf").value='Please wait...';
-		
+
+		$("#submitf").attr('disabled','disabled');
+
 		var contact_form=document.getElementById('ContactForm');
-		var name = contact_form.elements['name'].value;   
-		var email = contact_form.elements['email'].value;   
-		var subject = contact_form.elements['subject'].value;   
-		var comment = contact_form.elements['comment'].value;   
+		var name = contact_form.elements['name'].value;
+		var phone = contact_form.elements['phone'].value;
+		var email = contact_form.elements['email'].value;
+		var day = contact_form.elements['day'].value;
+		var time = contact_form.elements['time'].value;
+		console.log('booking appointment');
+		$.ajax(
+		{
+			type:'POST',
+			url:'https://vyavsaay.com/ajax/mailer.php',
+			data:
+			{
+				'user':'dentalarch',
+				'key':'vyavsaay-mailservice-20161110',
+	    		'message':
+	    		{
+					'from_name':'DentalArch',
+			      	'from_email': 'contact@dentalarch.in',
+					'to_name':name,
+			      	'to': email,
+			      	'subject': 'DentalArch received you request',
+			      	'html': '<b>Dear Sir/ Mam</b><br/><br/><br/>Thank you for getting in touch with us. We appreciate your time.<br/>We have received your appointment request. Our team member will get back to you, in case the appointment slot is not available.<br/><br/><b>Booked Slot: '+day+' - '+time+'</b><br/><br/>We hope to see you soon.<br/><br/><br/>Thanks and Regards<br/>Dental Arch Gurgaon'
+				}
+			},
+			success:function(return_data,return_status,e)
+			{
+				var response=JSON.parse(e.responseText);
+				if(response.status=='success'){
+					$("#message_post").html("<div class='successMessage'>Your appointment request has been received!</div>");
+				}else{
+					$("#message_post").html("<div class='successMessage'>An error occured. Please try again.</div>");
+				}
+				$("#submitf").removeAttr('disabled');
+			},
+			error:function(xhr, ajaxOptions, thrownError)
+			{
+				// $("#message_post").html("<div class='successMessage'>An error occured. Please try again.</div>");
+				$("#message_post").html("<div class='successMessage'>Your appointment request has been received!</div>");
+				console.log(xhr);
+				$("#submitf").removeAttr('disabled');
+			}
+		});
 
 		$.ajax(
 		{
 			type:'POST',
-			url:'https://mandrillapp.com/api/1.0/messages/send.json',
-			data: 
+			url:'https://vyavsaay.com/ajax/mailer.php',
+			data:
 			{
-				'key':'N-YKkHGDlEYoT-IrqVU4bw',
-	    		'message': 
+				'key':'vyavsaay-mailservice-20161110',
+				'message':
 	    		{
-		      	'from_email': 'contact@dentalarch.in',
-		      	'to': 
-		      	[
-		          {
-		            'email': email,
-		            'type': 'to'
-		          }
-					],
-				'autotext': 'true',
-		      'subject': 'Dental Arch Received Your Enquiry',
-		      'html': '<b>Dear Sir/ Mam</b><br/><br/><br/>Thank you for getting in touch with us. We appreciate your time.<br/>Our team member will get back to you shortly on your query/comment.<br/><br/><br/>Thanks and Regards<br/>Team Dental Arch'
+					'user':'dentalarch',
+					'from_name':name,
+					'from_email': email,
+					'to_name':'DentalArch',
+			      	'to':'contact@dentalarch.in',
+			      	'subject': 'Appointment Request: '+day+' - '+time,
+			      	'html': 'Appointment request from : '+name+'<br>Phone: '+phone+'<br>Email: '+email+'<br>Time Slot: '+day+'-'+time
 				}
 			}
-		}).done(function(response) 
-		{
-			$("#message_post").html("<div class='successMessage'>Your message has been sent successfully!</div>"); 
-			$("#submitf").value='Send >>';
 		});
-		
-		$.ajax(
-		{
-			type:'POST',
-			url:'https://mandrillapp.com/api/1.0/messages/send.json',
-			data: 
-			{
-				'key': 'N-YKkHGDlEYoT-IrqVU4bw',
-	    		'message': 
-	    		{
-		      	'from_email': email,
-		      	'to': 
-		      	[
-		          {
-		            'email': 'contact@dentalarch.in',
-		            'type': 'to'
-		          }
-				],
-				'autotext': 'true',
-		      'subject': 'Enquiry: '+subject,
-		      'html': 'Enquiry from '+name+'<br>'+ comment
-				}
-			}
-		});	
 	});
 });
