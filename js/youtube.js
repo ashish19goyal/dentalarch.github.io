a38bd6093ff456c487360ad9b564f09a378ff5ce
Ashish Goyal <ashish.19goyal@gmail.com>
2017-07-01 22:29:23 +0530

    Automated

---
+++ js/youtube.js
@@ -0,0 +1,36 @@
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+//after the API code downloads.
+var player;
+function onYouTubeIframeAPIReady() {
+player = new YT.Player('player', {
+	height: '390',
+	width: '640',
+	videoId: 'u-LzwEkiJ4Q',
+	events:
+	{
+		'onReady': onPlayerReady,
+		'onStateChange': onPlayerStateChange
+	}
+});
+}
+
+function onPlayerReady(event)
+{
+	event.target.playVideo();
+}
+
+var done = false;
+function onPlayerStateChange(event)
+{
+	if (event.data == YT.PlayerState.PLAYING && !done) {
+	  setTimeout(stopVideo, 6000);
+	  done = true;
+	}
+}
+function stopVideo() {
+	player.stopVideo();
+}
